9b8833e3ea039d805360759cf246db62829dd9fc
AI Engineer <iqbal@Bahasas-MBP-2.mshome.net>
2022-10-27 12:27:37 +0700

    refine the look

--- src/ocr/ocr_render.js
+++ src/ocr/ocr_render.js
@@ -3,6 +3,7 @@ extractBtn.addEventListener("click", async function() {
     let data = new FormData();
     let image = $("#image")[0].files[0];
     data.append('image', image);
+
     const host = await getHost();
 
     axios.post(host + '/', data, {
@@ -31,12 +32,25 @@ async function getHost() {
 function previewImage(event) {
     let image = URL.createObjectURL(event.target.files[0]);
     let imagediv = document.querySelector("#preview");
+    imagediv.innerHTML = "";
     let newimage = document.createElement("img");
+    newimage.classList.add(...["mh-100", "mw-100"]);
     newimage.src = image;
-    if (imagediv.firstElementChild == null) {
+    if (!imageLoaded()) {
         imagediv.appendChild(newimage);
+        enableExtractBtn();
     } else {
         let oldImage = imagediv.firstElementChild;
         oldImage.parentNode.replaceChild(newimage, oldImage);
     }
+}
+
+function imageLoaded() {
+    let preview = document.querySelector("#preview");
+    return preview.firstElementChild != null;
+}
+
+function enableExtractBtn() {
+    extractBtn.disabled = false;
+    extractBtn.classList.replace("btn-secondary", "btn-info")
 }
